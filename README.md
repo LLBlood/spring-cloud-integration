2ab070c120a8929a459bc45c1d4c6ffdac1f8fa9
liulin <liulin@gmail.com>
2021-02-19 16:08:10 +0800

    stpry# SpringCloud eureka init

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ SpringCloud模块聚合学习
 详见创建过程：
 
 [创建父类maven工程](https://blog.csdn.net/qq_43479628/article/details/113863725)
+
+### 二、创建注册中心eureka工程
+
+其中pom内容如pom.xml所示
+
+详见创建过程：
+
+[创建注册中心eureka工程](https://blog.csdn.net/qq_43479628/article/details/113865942)
+
---
+++ plateform/spring-cloud-eureka/src/main/java/cn/ll/springcloudeureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.ll.springcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+
+}
