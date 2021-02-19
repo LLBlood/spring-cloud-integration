efe2502baf15a8bd812a484dbc289fdbb6ff29cf
liulin <liulin@gmail.com>
2021-02-19 16:38:37 +0800

    story# eureka工程更改包名

--- plateform/spring-cloud-eureka/src/main/java/cn/liulin/springcloudeureka/SpringCloudEurekaApplication.java
+++ plateform/spring-cloud-eureka/src/main/java/cn/liulin/springcloudeureka/SpringCloudEurekaApplication.java
@@ -1,4 +1,4 @@
-package cn.ll.springcloudeureka;
+package cn.liulin.springcloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- plateform/spring-cloud-eureka/src/test/java/cn/liulin/springcloudeureka/SpringCloudEurekaApplicationTests.java
+++ plateform/spring-cloud-eureka/src/test/java/cn/liulin/springcloudeureka/SpringCloudEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.ll.springcloudeureka;
+package cn.liulin.springcloudeureka;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
